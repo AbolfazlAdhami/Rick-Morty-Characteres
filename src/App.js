5308f2ab09dfb6b888c4fcbf16414f89f47da6c8
AbolfazlAdhami <aadhami880@gmail.com>
2024-12-20 00:32:26 +0330

    update: install tailwind update react-query

--- src/App.js
+++ src/App.js
@@ -2,10 +2,7 @@ import React from "react";
 import MainPage from "./Container/MainPage";
 import Wrapper from "./Container/HOC/Wrapper";
 import "./App.css";
-import { Container } from "react-bootstrap";
-// Font MUI
-
-// Font MUI
+ 
 function App() {
   return (
     <Wrapper>
